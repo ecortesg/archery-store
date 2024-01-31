2cd159beb164138af670b298420192887b90ad3a
ecortesg <cgarcia.ernesto@gmail.com>
2024-01-30 21:11:02 -0600

    Transformed date from UTC to local

--- src/scenes/account/OrdersTable.jsx
+++ src/scenes/account/OrdersTable.jsx
@@ -59,7 +59,7 @@ function Row({ row }) {
           </IconButton>
         </TableCell>
         <TableCell>{row.uuid}</TableCell>
-        <TableCell>{row.created_at}</TableCell>
+        <TableCell>{new Date(row.created_at).toLocaleString()}</TableCell>
         <TableCell>Pending</TableCell>
         <TableCell align="right">${row.total}</TableCell>
       </TableRow>
